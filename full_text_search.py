bf006f7fa551922eb04ba23c17bfd967191f5a4f
zhangzhong <im.zhong@outlook.com>
2025-11-15 23:42:17 +0800

    learn full text search

---
+++ full_text_search.py
@@ -0,0 +1,11 @@
+# 2025/11/15
+# zhangzhong
+# https://www.elastic.co/docs/solutions/search/full-text
+# 这里面有很多的相关资源
+# full text search
+
+# Documents and search queries are transformed to enable returning relevant results instead of simply exact term matches.
+# Fields of type text are analyzed and indexed for full-text search.
+# Because it runs efficiently on CPUs, Elasticsearch's full-text search requires minimal computational resources compared to GPU-intensive vector operations.
+
+
